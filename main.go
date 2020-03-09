6c72dc2eb4c0421d9370b5d6aebe1f981a73a832
Brennon Loveless <bloveless1@wayfair.com>
2020-03-09 11:27:03 -0400

    Parallel logs are working. Added first argument so users could specify their namespace

--- main.go
+++ main.go
@@ -1,14 +1,12 @@
 package main
 
 import (
-	// "context"
-	"bytes"
 	"flag"
 	"fmt"
 	"io"
 	"os"
 	"path/filepath"
-	"time"
+	"sync"
 
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -19,6 +17,13 @@ import (
 )
 
 func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Please provide the namespace as the first argument")
+	}
+
+	namespace := os.Args[1]
+	fmt.Printf("Tailing pods in namespace %s\n", namespace)
+
 	var kubeconfig *string
 	if home := homeDir(); home != "" {
 		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
@@ -38,38 +43,50 @@ func main() {
 	if err != nil {
 		panic(err.Error())
 	}
+
+	pods, err := clientset.CoreV1().Pods(namespace).List(metav1.ListOptions{})
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Printf("There are %d pods in the namespace %s\n", len(pods.Items), namespace)
+
+	var wg sync.WaitGroup
+	for _, pod := range pods.Items {
+		wg.Add(1)
+		go echoPodLogs(clientset, namespace, pod.Name, &wg)
+	}
+
+	wg.Wait()
+}
+
+func echoPodLogs(clientset *kubernetes.Clientset, namespace string, podName string, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	sinceSeconds := int64(30)
+	plo := &v1.PodLogOptions{
+		Follow:       true,
+		SinceSeconds: &sinceSeconds,
+	}
+	req := clientset.CoreV1().Pods(namespace).GetLogs(podName, plo)
+	podLogs, err := req.Stream()
+	if err != nil {
+		panic("error in opening string")
+	}
+	defer podLogs.Close()
+
 	for {
-		namespace := "buyfair-refresh-cli"
-		pods, err := clientset.CoreV1().Pods(namespace).List(metav1.ListOptions{})
-		if err != nil {
-			panic(err.Error())
-		}
-		fmt.Printf("There are %d pods in the namespace %s\n", len(pods.Items), namespace)
-
-		for _, pod := range pods.Items {
-			fmt.Printf("Name: %s\n", pod.Name)
-
-			req := clientset.CoreV1().Pods(namespace).GetLogs(pod.Name, &v1.PodLogOptions{})
-			podLogs, err := req.Stream()
-			if err != nil {
-				panic("error in opening string")
-			}
-			defer podLogs.Close()
-
-			for {
-				buf := new(bytes.Buffer)
-				_, ioErr := io.Copy(buf, podLogs)
-				if ioErr != nil {
-					panic("error in copy information from podLogs to buf")
-				}
-
-				str := buf.String()
-
-				fmt.Println(str)
-			}
+		buf := make([]byte, 256)
+		n, err := podLogs.Read(buf)
+		if err != nil && err == io.EOF {
+			fmt.Println("EOF... done")
+			break
 		}
 
-		time.Sleep(10 * time.Second)
+		if n == 0 {
+			continue
+		}
+
+		fmt.Printf("[%s] %s\n", podName, string(buf[0:n]))
 	}
 }
 
