fa570b67ac7ebb185e80edd9debdfadd7615a434
Brennon Loveless <bloveless1@wayfair.com>
2020-03-23 09:43:47 -0400

    Started experimenting with a terminal ui

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module kube-logger-go
 go 1.13
 
 require (
+	github.com/gdamore/tcell v1.3.0
+	github.com/gizak/termui/v3 v3.1.0
 	k8s.io/api v0.17.0
 	k8s.io/apimachinery v0.17.0
 	k8s.io/client-go v0.17.0
--- main.go
+++ main.go
@@ -4,10 +4,13 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"math/rand"
 	"os"
 	"path/filepath"
 	"sync"
+	"time"
 
+	"github.com/gdamore/tcell"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
@@ -16,7 +19,179 @@ import (
 	_ "k8s.io/client-go/plugin/pkg/client/auth/oidc"
 )
 
+func makebox(s tcell.Screen) {
+	w, h := s.Size()
+
+	if w == 0 || h == 0 {
+		return
+	}
+
+	glyphs := []rune{'@', '#', '&', '*', '=', '%', 'Z', 'A'}
+
+	lx := rand.Int() % w
+	ly := rand.Int() % h
+	lw := rand.Int() % (w - lx)
+	lh := rand.Int() % (h - ly)
+	st := tcell.StyleDefault
+	gl := ' '
+	if s.Colors() > 256 {
+		rgb := tcell.NewHexColor(int32(rand.Int() & 0xffffff))
+		st = st.Background(rgb)
+	} else if s.Colors() > 1 {
+		st = st.Background(tcell.Color(rand.Int() % s.Colors()))
+	} else {
+		st = st.Reverse(rand.Int()%2 == 0)
+		gl = glyphs[rand.Int()%len(glyphs)]
+	}
+
+	for row := 0; row < lh; row++ {
+		for col := 0; col < lw; col++ {
+			s.SetCell(lx+col, ly+row, st, gl)
+		}
+	}
+	s.Show()
+}
+
 func main() {
+	tcell.SetEncodingFallback(tcell.EncodingFallbackASCII)
+	s, e := tcell.NewScreen()
+	if e != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", e)
+		os.Exit(1)
+	}
+	if e = s.Init(); e != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", e)
+		os.Exit(1)
+	}
+
+	s.SetStyle(tcell.StyleDefault.
+		Foreground(tcell.ColorBlack).
+		Background(tcell.ColorWhite))
+	s.Clear()
+
+	quit := make(chan struct{})
+	go func() {
+		for {
+			ev := s.PollEvent()
+			switch ev := ev.(type) {
+			case *tcell.EventKey:
+				switch ev.Key() {
+				case tcell.KeyEscape, tcell.KeyEnter:
+					close(quit)
+					return
+				case tcell.KeyCtrlL:
+					s.Sync()
+				}
+			case *tcell.EventResize:
+				s.Sync()
+			}
+		}
+	}()
+
+	cnt := 0
+	dur := time.Duration(0)
+
+loop:
+	for {
+		select {
+		case <-quit:
+			break loop
+		case <-time.After(time.Millisecond * 50):
+		}
+		start := time.Now()
+		makebox(s)
+		cnt++
+		dur += time.Now().Sub(start)
+	}
+
+	s.Fini()
+	fmt.Printf("Finished %d boxes in %s\n", cnt, dur)
+	fmt.Printf("Average is %0.3f ms / box\n", (float64(dur)/float64(cnt))/1000000.0)
+}
+
+// func main() {
+// 	if err := ui.Init(); err != nil {
+// 		log.Fatalf("failed to initialize termui: %v", err)
+// 	}
+// 	defer ui.Close()
+//
+// 	width, height := ui.TerminalDimensions()
+//
+// 	col := false
+//
+// 	drawFunction := func() {
+// 		if col {
+// 			colWidth := width / 3
+//
+// 			p := widgets.NewParagraph()
+// 			p.Title = "This is a title that is clearly too long for this window"
+// 			p.Text = "This is text that is clearly too long for this window"
+// 			p.SetRect(0, 0, colWidth, height)
+//
+// 			p2 := widgets.NewParagraph()
+// 			p2.Title = "This is a title that is clearly too long for this window"
+// 			p2.Text = "This is text that is clearly too long for this window"
+// 			p2.SetRect(colWidth, 0, colWidth*2, height)
+//
+// 			p3 := widgets.NewParagraph()
+// 			p3.Title = "This is a title that is clearly too long for this window"
+// 			p3.Text = "This is text that is clearly too long for this window"
+// 			p3.SetRect(colWidth*2, 0, colWidth*3, height)
+//
+// 			ui.Render(p, p2, p3)
+// 		} else {
+// 			rowHeight := height / 3
+//
+// 			p := widgets.NewParagraph()
+// 			p.Title = "This is a title that is clearly too long for this window"
+// 			p.Text = "This is text that is clearly too long for this window"
+// 			p.SetRect(0, 0, width, rowHeight)
+//
+// 			p2 := widgets.NewParagraph()
+// 			p2.Title = "This is a title that is clearly too long for this window"
+// 			p2.Text = "This is text that is clearly too long for this window"
+// 			p2.SetRect(0, rowHeight, width, rowHeight*2)
+//
+// 			p3 := widgets.NewParagraph()
+// 			p3.Title = "This is a title that is clearly too long for this window"
+// 			p3.Text = "This is text that is clearly too long for this window"
+// 			p3.SetRect(0, rowHeight*2, width, height)
+//
+// 			ui.Render(p, p2, p3)
+// 		}
+//
+// 	}
+//
+// 	uiEvents := ui.PollEvents()
+// 	ticker := time.NewTicker(time.Second).C
+// 	for {
+// 		select {
+// 		case e := <-uiEvents:
+// 			switch e.ID { // event string/identifier
+// 			case "q", "<C-c>": // press 'q' or 'C-c' to quit
+// 				return
+// 			case "<Space>":
+// 				col = !col
+// 				// case "<MouseLeft>":
+// 				// 	payload := e.Payload.(ui.Mouse)
+// 				// 	x, y := payload.X, payload.Y
+// 				// case "<Resize>":
+// 				// 	payload := e.Payload.(ui.Resize)
+// 				// 	width, height = payload.Width, payload.Height
+// 				// }
+// 				// switch e.Type {
+// 				// case ui.KeyboardEvent: // handle all key presses
+// 				// 	eventID := e.ID // keypress string
+// 				// 	fmt.Printf("<keyboard> eventID: %v", eventID)
+// 			}
+// 		// use Go's built-in tickers for updating and drawing data
+// 		case <-ticker:
+// 			drawFunction()
+// 		}
+// 	}
+// }
+
+func realMain() {
 	if len(os.Args) < 2 {
 		fmt.Println("Please provide the namespace as the first argument")
 	}
